054471176ffbaa0342ccacb450b9a3ca5cdbaf8b
Батырбек Айгалиев <www.bat.94@gmail.com>
2023-09-03 12:28:17 +0500

    add google authetication

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+
+"use client";
+
+import React, { useState, useEffect } from 'react'
+import { getProviders, signIn } from 'next-auth/react';
+
+type Provider = {
+  id: string,
+  name: string,
+  type: string,
+  signinUrl: string,
+  callbackUrl: string,
+  signinUrlParams?: Record<string, string> | null,
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProvider = () => {
-  return (
-    <div>AuthProvider</div>
-  )
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const providers = await getProviders();
+
+      setProviders(providers);
+    }
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {
+          Object.values(providers).map((provider: Provider, i) => (
+            <button key={i} onClick={() => signIn(provider.id)}>{provider.id}</button>
+          ))
+        }
+      </div>
+    );
+  }
 }
 
 export default AuthProvider
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,11 @@ import Image from 'next/image';
 import React from 'react'
 import { NavLinks } from '@/constants';
 import AuthProvider from './AuthProvider';
+import { getCurrentUser } from '@/lib/session';
 
-const Navbar = () => {
-  const session = null;
+const Navbar = async () => {
+  const session = await getCurrentUser();
+  
   return (
     <nav className='flexBetween navbar'>
       <div className='flex-1 flexStart gap-10'>
@@ -29,9 +31,15 @@ const Navbar = () => {
       </div>
       <div className='flexCenter gap-4'>
         {
-          session ? (
+          session?.user?.image ? (
             <>
-              UserPhoto
+              <Image
+                src={session.user.image}
+                width={40}
+                height={40}
+                className='rounded-full'
+                alt={session.user.name}
+              />
               <Link href='/create-project' >
                 Share work
               </Link>
